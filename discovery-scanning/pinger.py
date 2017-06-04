74c97cb387c75c5e158458579a76866cf83663be
root <root@localhost.localdomain>
2017-06-04 22:38:32 +0700

    add pinger

---
+++ discovery-scanning/pinger.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import logging
+import subprocess
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from scapy.all import *
+
+if len(sys.argv) != 2:
+    print "Usage - ./pinger.py [/24 network address]"
+    print "Example - ./pinger.py 192.168.0.0"
+    print "Example will perform an ICMP scan of the 192.168.0.0/24 range"
+    sys.exit()
+
+address = str(sys.argv[1])
+prefix = address.split('.')[0] + '.' + address.split('.')[1] + '.' + address.split('.')[2] + '.'
+
+for addr in range(1,254):
+    answer = sr1(ARP(pdst=prefix+str(addr)),timeout=1,verbose=0)
+    if answer == None:
+        pass
+    else:
+        print prefix+str(addr)
---
+++ discovery-scanning/pinger_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import logging
+import subprocess
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from scapy.all import *
+
+if len(sys.argv) != 2:
+    print "Usage - ./pinger.py [filename]"
+    print "Example - ./pinger.py iplist.txt"
+    print "Example will perform an ICMP scan of the IP addresses in iplist.txt"
+    sys.exit()
+
+filename = str(sys.argv[1])
+file = open(filename, 'r')
+
+for addr in file:
+    answer = sr1(IP(dst=addr.strip())/ICMP(),timeout=1,verbose=0)
+    if answer == None:
+        pass
+    else:
+        print addr.strip()
